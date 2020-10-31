10d1ab5237b4eb0fcd822fead32a58096d0c0ef2
Akash Madhu <akashmadhu4@gmail.com>
2020-10-31 15:54:12 +0530

    finished setting up certificate component

--- src/Components/Certificates.js
+++ src/Components/Certificates.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React , {useState} from "react";
+import './Certificates.css';
 import {Card,CardImg} from 'reactstrap';
-import myimage from '../images/myimage.png';
-import pics from '../images/pics.jpg'
+import Reactcoursera from '../images/Reactcoursera.jpg';
+import bootstrap4 from '../images/bootstrap4coursera.jpg';
+import responsive from '../images/courserawebdesgin.jpg';
+import coursera4 from '../images/coursera4.jpg';
+
+const RenderCard=(props)=>{
+    return(         
+            <a>
+                <Card className="cert_card">
+                    <CardImg src={props.image}/>
+                </Card> 
+            </a>     
+    );
+}
+
 
 function Certificates(){
+    const [images] = useState([Reactcoursera,bootstrap4,responsive,coursera4])
+    const certificates=images.map((image)=>{
+        return(
+            <div className="col-12 mb-5 col-md-4">
+                <RenderCard image={image}/>
+            </div>
+        );
+    })
     return(
-        <section id="certificates">
-            <div className="cert__body">
-                <h1 className="heading">Certificates</h1>     
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-4">
-                            <div className="my-card">
-                                <img src={pics}></img>
-                            </div>
-                        </div>
+        <section id="certificates">               
+            <div className="container">
+                <div className="row cert_row">
+                    <div className="col-12 d-flex justify-content-center">
+                        <h1 className="cert_heading">My Certificates</h1>
                     </div>
                 </div>
+                <div className="row  cert__row">
+                    {certificates}
+                </div>    
             </div>
         </section>            
     );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,24 +4,24 @@ import './Navbar.css';
 function Navbar(){
     return(
         <div>
-            <div className="navbar navbar-expand-lg navbar-dark bg-dark" role="navigation">
+            <div className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav" role="navigation">
                 <a className="navbar-brand nav_text"href="#">Akash Madhu</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active ">
-                            <a className="nav-link nav__listitems nav_text" href="#">Home <span class="sr-only">(current)</span></a>
+                        <li className="nav-item active js-scroll-trigger ">
+                            <a className="nav-link nav__listitems nav_text" href="#home">Home <span class="sr-only">(current)</span></a>
                         </li>
-                        <li className="nav-item ">
+                        <li className="nav-item js-scroll-trigger">
                             <a className="nav-link nav__listitems nav_text" href="#">About Me</a>
                         </li>
-                        <li className="nav-item ">
+                        <li className="nav-item js-scroll-trigger">
                             <a className="nav-link nav__listitems nav_text" href="#"> My Projects</a>
                         </li>
-                        <li className="nav-item ">
-                            <a className="nav-link nav__listitems nav_text" href="#"> Certificates</a>
+                        <li className="nav-item js-scroll-trigger ">
+                            <a className="nav-link nav__listitems nav_text" href="#certificates"> Certificates</a>
                         </li>
                     </ul>
                 </div>
